790c5a7d9b44f4053602fb41c4fab0cbc2af43c7
Arul2301 <71940931+Arul2301@users.noreply.github.com>
2024-12-12 22:26:39 +0530

    Prime_Numbers_In_N
    Write a program that gets n as input and print all the prime numbers till that number.

---
+++ PrimeNumbersIn_N.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+import static java.lang.Math.sqrt;
+
+public class PrimeNumbersIn_N {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int N = in.nextInt();
+        for(int v=2;v<=N;v++){
+            int flag =0;
+            for(int i=2;i<v;i++){
+                if(v%i==0){
+                    flag=1;
+                }
+            }
+            if(flag==0) {
+                System.out.print(v+" ");
+            }
+
+        }
+
+    }
+
+}
